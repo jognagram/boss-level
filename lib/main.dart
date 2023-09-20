4c61d537ddc745ea1539b9567ec1ba20d592f662
jogna <jordan.gnamien@smile.ci>
2023-09-20 18:58:18 +0000

    i got it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Ask Me Anything",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ask Me Anything',
+          ),
+          backgroundColor: Colors.lightBlue.shade900,
+        ),
+        body: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int myball = 1;
+  void change() {
+    setState(() {
+      myball = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlue.shade800,
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            change();
+          },
+          child: Image.asset('images/ball$myball.png'),
+        ),
+      ),
     );
   }
 }
